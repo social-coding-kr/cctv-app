79a2d578be12015048f697a4b81165334f489e2b
sky1core <sky1core@gmail.com>
2015-08-03 12:59:51 +0900

    build config modified for jenkins

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 pushd cctvApp
 
 mkdir -p buildlog
-#ionic platform add android
+ionic platform add android
 ionic build android > buildlog/lastbuild.log
 cat buildlog/lastbuild.log
 grep "BUILD SUCCESS" buildlog/lastbuild.log
