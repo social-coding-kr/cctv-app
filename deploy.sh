b0eb9682a164b981649168d77f43d05f30c30586
sky1core <sky1core@gmail.com>
2015-08-03 16:35:22 +0900

    add deploy config

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pushd cctvApp
+echo $1
+
+ps -ef | grep "ionic serve -p $1" | grep -v "grep"
+not_found=$?
+echo not_found
+
+if [ not_found = 0 ]; then
+    echo "KILL exist serve process"
+    
+fi
+
+screen -d -m -L ionic serve -p $1 --nolivereload
+
+
+#exit $?
