709eb839f084975c729c4d65fb329ba459d62f7d
sky1core <sky1core@gmail.com>
2015-08-03 12:57:46 +0900

    platform add android

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 pushd cctvApp
 
 mkdir -p buildlog
+#ionic platform add android
 ionic build android > buildlog/lastbuild.log
 cat buildlog/lastbuild.log
 grep "BUILD SUCCESS" buildlog/lastbuild.log
