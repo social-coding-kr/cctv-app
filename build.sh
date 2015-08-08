e9425f4b992e79abf43973e8b7cfa97197ae9705
sky1core <sky1core@gmail.com>
2015-08-09 03:37:16 +0900

    Add Plugins

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ pushd cctvApp
 mkdir -p buildlog
 ionic platform add android
 ionic plugin add https://github.com/apache/cordova-plugin-whitelist.git
+ionic plugin add cordova-plugin-file
+ionic plugin add cordova-plugin-file-transfer
+ionic plugin add https://github.com/pwlin/cordova-plugin-file-opener2.git
+
 
 ionic build android > buildlog/lastbuild.log
 cat buildlog/lastbuild.log
