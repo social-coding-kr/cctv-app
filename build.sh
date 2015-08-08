bb8cce4e2bab232dda9649bd7505bb831b33bfba
sky1core <sky1core@gmail.com>
2015-08-08 21:23:41 +0900

    Add Cordova WhiteList Plugin

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ pushd cctvApp
 
 mkdir -p buildlog
 ionic platform add android
+ionic plugin add https://github.com/apache/cordova-plugin-whitelist.git
+
 ionic build android > buildlog/lastbuild.log
 cat buildlog/lastbuild.log
 grep "BUILD SUCCESS" buildlog/lastbuild.log
