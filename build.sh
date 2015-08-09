d2ce7c1281a2f97fbd25c7b314b031bc82a9ddfd
sky1core <sky1core@gmail.com>
2015-08-09 10:45:37 +0900

    안드로이드 개발용 임시 키 파일 및 빌드 설정 추가

--- build.sh
+++ build.sh
@@ -10,8 +10,21 @@ ionic plugin add cordova-plugin-file-transfer
 ionic plugin add https://github.com/pwlin/cordova-plugin-file-opener2.git
 
 
-ionic build android > buildlog/lastbuild.log
+ionic build --release android > buildlog/lastbuild.log
 cat buildlog/lastbuild.log
+
+# 개발용 key
+# key_store : my-devel-key.keystore
+# alias : alias_name
+# password : devel_pass
+# keytool -genkey -v -keystore my-devel-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+
+
+jarsigner -verbose -storepass devel_pass -sigalg SHA1withRSA -digestalg SHA1 -keystore my-devel-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+$ANDROID_HOME/build-tools/22.0.1/zipalign -f -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/cctvApp.apk
+jarsigner -verify -verbose -certs platforms/android/build/outputs/apk/cctvApp.apk
+
+
 grep "BUILD SUCCESS" buildlog/lastbuild.log
 
 exit $?
