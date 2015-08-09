a194066f0b17a858c07b1e43fdcd0cc186be7c1f
sky1core <sky1core@gmail.com>
2015-08-09 18:44:57 +0900

    Build 와 Setup을 분리

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd cctvApp
+
+ionic platform add android
+ionic plugin add https://github.com/apache/cordova-plugin-whitelist.git
+ionic plugin add cordova-plugin-file
+ionic plugin add cordova-plugin-file-transfer
+ionic plugin add https://github.com/pwlin/cordova-plugin-file-opener2.git
+
