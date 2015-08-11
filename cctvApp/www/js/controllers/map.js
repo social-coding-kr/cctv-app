bd1e612fbabcf01193c2fdfa7f6dbae50c9baae8
cosi1223 <cosi1223@hotmail.com>
2015-08-11 15:50:04 +0900

    지도 화면에 띄우기 및 자신의 위치 찾기-구현중

--- cctvApp/www/js/controllers/map.js
+++ cctvApp/www/js/controllers/map.js
@@ -1,11 +1,61 @@
 'use strict';
 angular.module('starter.controllers')
 
-.controller('MapCtrl', function($scope) {
-  $scope.test_button2_text = '눌러봐'; // 초기값 
+.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
+  
+   function initialize() {
+   var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+        
+   var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+   };
+   var map = new google.maps.Map(document.getElementById("map"),
+      mapOptions);
+        
+   //Marker + infowindow + angularjs compiled ng-click
+   var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+   var compiled = $compile(contentString)($scope);
 
-  $scope.test_button2_click = function() {
-    $scope.applog("개발자용2 화면에서 로그를 남겼습니다. javascript에서 string 줄바꿈은 도대체 어떻게 하는 걸까요?");
+   var infowindow = new google.maps.InfoWindow({
+      content: compiled[0]
+    });
 
-  }
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Uluru (Ayers Rock)'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+
+    $scope.map = map;
+    }
+      
+    google.maps.event.addDomListener(window, 'load', initialize);
+      
+    $scope.centerOnMe = function() {
+        if(!$scope.map) {
+          return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+          content: 'Getting current location...',
+          showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error) {
+          alert('Unable to get location: ' + error.message);
+        });
+    };
+      
+    $scope.clickTest = function() {
+        alert('Example of infowindow with ng-click')
+    };
 })
