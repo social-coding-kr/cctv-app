c5e2f8f2edb359f65237ba48e8a57491d0951f00
sky1core <sky1core@gmail.com>
2015-08-16 16:54:59 +0900

    토스트 플러그인 및 테스트코드 추가

--- cctvApp/www/js/controllers/devel2.js
+++ cctvApp/www/js/controllers/devel2.js
@@ -1,12 +1,22 @@
 'use strict';
 angular.module('starter.controllers')
 
-.controller('DevelCtrl2', function($scope, soc) {
+.controller('DevelCtrl2', function($scope, soc, $cordovaToast, $location, $window) {
   $scope.test_button2_text = '눌러봐'; // 초기값 
 
   $scope.test_button2_click = function() {
     soc.log("개발자용2 화면에서 로그를 남겼습니다. javascript에서 string 줄바꿈은 도대체 어떻게 하는 걸까요?");
 
+  $cordovaToast
+    .show('Here is a message', 'long', 'bottom')
+    .then(function(success) {
+      // success
+    }, function (error) {
+      // error
+    });
+
+      $location.path('/app/map');
+      $window.location.reload();
   }
   
 
