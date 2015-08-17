96a7a462f1ab53182b17bb407265b5d06fc08fb0
sky1core <sky1core@gmail.com>
2015-08-17 12:34:06 +0900

    지도표시 코드/스타일 수정, ion-content scroll 속성 수정

--- cctvApp/www/js/controllers/map.js
+++ cctvApp/www/js/controllers/map.js
@@ -5,6 +5,10 @@ angular.module('starter.controllers')
   
       //function init() {
       	 var map = L.map('map');
+      	 var curLoc = soc.getDefaultLocation();
+         var Seoul = new L.LatLng(curLoc.lat, curLoc.lon); // geographical point (longitude and latitude)
+         map.setView(Seoul, 15);
+      	 
       	 
          L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
@@ -12,8 +16,6 @@ angular.module('starter.controllers')
          }).addTo(map);
          map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
 
-         var Seoul = new L.LatLng(37.555107, 126.970691); // geographical point (longitude and latitude)
-         map.setView(Seoul, 13);
                
          //var marker = L.marker([37.555107, 126.970691]).addTo(map);
          //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
