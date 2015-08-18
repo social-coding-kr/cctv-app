d385ed0f6c25ac1c85bc334408f0d12679c4c622
sky1core <sky1core@gmail.com>
2015-08-18 09:26:49 +0900

    LegacyLog 삭제

--- cctvApp/www/js/controllers/devel2.js
+++ cctvApp/www/js/controllers/devel2.js
@@ -5,7 +5,7 @@ angular.module('starter.controllers')
   $scope.test_button2_text = '눌러봐'; // 초기값 
 
   $scope.test_button2_click = function() {
-    soc.log("개발자용2 화면에서 로그를 남겼습니다. javascript에서 string 줄바꿈은 도대체 어떻게 하는 걸까요?");
+    soc.log("개발자용2 화면에서 로그를 남겼습니다.");
 
   $cordovaToast
     .show('Here is a message', 'long', 'bottom')
