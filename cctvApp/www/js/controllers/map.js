db3f6d522fc9fa58aaef645fb9657df23f391b65
cosi1223 <cosi1223@hotmail.com>
2015-08-19 20:35:54 +0900

    지도에 자신의 위치 표시 및 중복되는 위치 표시 문제의 해결

--- cctvApp/www/js/controllers/map.js
+++ cctvApp/www/js/controllers/map.js
@@ -2,28 +2,29 @@
 angular.module('starter.controllers')
 
 .controller('MapCtrl', function($rootScope, $scope, $ionicLoading, $compile, soc) {
-  
-      //function init() {
-      	 var map = L.map('map');
-      	 var curLoc = soc.getDefaultLocation();
-         var Seoul = new L.LatLng(curLoc.lat, curLoc.lon); // geographical point (longitude and latitude)
-         map.setView(Seoul, 15);
-      	 
-      	 
-         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-            maxZoom: 18
-         }).addTo(map);
-         map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
 
-               
-         //var marker = L.marker([37.555107, 126.970691]).addTo(map);
-         //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-        
-         $scope.map = map;
-      //}
+    var map = L.map('map');
+    var curLoc = soc.getDefaultLocation();
+    var Seoul = new L.LatLng(curLoc.lat, curLoc.lon); // geographical point (longitude and latitude)
+    map.setView(Seoul, 15);
+      	 
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+        maxZoom: 18
+    }).addTo(map);
+    
+    map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
+    $scope.map = map;
+    var markers = new L.FeatureGroup();
+    
+    function MyLocationMarker(Location) {
+        var marker = L.marker(Location);
+        markers.addLayer(marker);
+        map.addLayer(markers);
+        marker.bindPopup('You are here.')
+            .openPopup();
+    }
 
-      
     $rootScope.centerOnMe = function() {
         //soc.log("hahaha" + JSON.stringify($scope.map));
         if(!$scope.map) {
@@ -37,15 +38,13 @@ angular.module('starter.controllers')
 
         navigator.geolocation.getCurrentPosition(function(pos) {
         //soc.log(JSON.stringify($scope.map));
-          var Location = new L.LatLng(pos.coords.latitude, pos.coords.longitude);
-          $scope.map.setView(Location, 13);
-          $scope.loading.hide();
+            markers.clearLayers();
+            var Location = new L.LatLng(pos.coords.latitude, pos.coords.longitude);
+            MyLocationMarker(Location);
+            $scope.map.setView(Location, 15);
+            $ionicLoading.hide();
         }, function(error) {
-          alert('Unable to get location: ' + error.message);
+            alert('Unable to get location: ' + error.message);
         });
-    };
-      
-    $scope.clickTest = function() {
-        alert('Example of infowindow with ng-click')
     };
 })
