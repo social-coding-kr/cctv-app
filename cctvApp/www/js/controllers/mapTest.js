cc2f26ba06b5950aada8caa994b1fab4030df4bb
sky1core <sky1core@gmail.com>
2015-09-14 15:27:52 +0900

    확대축소/커스텀 컨트롤 추가 (+/-)이미지가 나오지 않음

--- cctvApp/www/js/controllers/mapTest.js
+++ cctvApp/www/js/controllers/mapTest.js
@@ -31,6 +31,28 @@ angular.module('starter.controllers')
 
 			// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 			var map = new daum.maps.Map(mapContainer, mapOption); 
+			
+			
+			// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+			//var mapTypeControl = new daum.maps.MapTypeControl();
+
+			// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+			// daum.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+			//map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
+
+			// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+			//var zoomControl = new daum.maps.ZoomControl();
+			//map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+			
+			// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+			$scope.zoomIn = function() {
+    			map.setLevel(map.getLevel() - 1);
+			}
+
+			// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+			$scope.zoomOut = function() {
+    			map.setLevel(map.getLevel() + 1);
+			}
 		});
 
 /*
