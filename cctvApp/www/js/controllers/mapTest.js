a0b46c083f37d3fac14f18c3d4a5955b6918f4f8
sky1core <sky1core@gmail.com>
2015-09-17 15:50:14 +0900

    MapTest 정리중

--- cctvApp/www/js/controllers/mapTest.js
+++ cctvApp/www/js/controllers/mapTest.js
@@ -6,39 +6,19 @@ angular.module('starter.controllers')
 
 		$ionicPlatform.ready(function() {
 
-            var mapContainer = document.getElementById('map2'), // 지도를 표시할 div 
-    		mapOption = { 
-        		center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+            var mapContainer = document.getElementById('map2'); // 지도를 표시할 div 
+            var defLoc = soc.getDefaultLocation();
+    		var mapOption = { 
+        		center: new daum.maps.LatLng(defLoc.lat, defLoc.lon), // 지도의 중심좌표
         		level: 3 // 지도의 확대 레벨
 		   	};
 
 			// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 			var map = new daum.maps.Map(mapContainer, mapOption); 
 			
-			
-			// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-			//var mapTypeControl = new daum.maps.MapTypeControl();
 
-			// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-			// daum.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-			//map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
 
-			// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-			//var zoomControl = new daum.maps.ZoomControl();
-			//map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
-			
-			// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-			$scope.zoomIn = function() {
-    			map.setLevel(map.getLevel() - 1);
-			}
-
-			// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-			$scope.zoomOut = function() {
-    			map.setLevel(map.getLevel() + 1);
-			}
-			
 			$scope.isDraging = false;
-			
 			$scope.refreshMapInfo = function() {
 			    $scope.mapInfoCenter = map.getCenter().toString();
 			    var bounds = map.getBounds();
@@ -60,6 +40,28 @@ angular.module('starter.controllers')
 
 			    $scope.requestInfoCenter = map.getCenter(); 
 			}
+
+			// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+			//var mapTypeControl = new daum.maps.MapTypeControl();
+
+			// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+			// daum.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+			//map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
+
+			// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+			//var zoomControl = new daum.maps.ZoomControl();
+			//map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+			
+			// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+			$scope.zoomIn = function() {
+    			map.setLevel(map.getLevel() - 1);
+			}
+
+			// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+			$scope.zoomOut = function() {
+    			map.setLevel(map.getLevel() + 1);
+			}
+			
 			
             daum.maps.event.addListener(map, 'dragstart', function() {
                 $scope.isDraging = true;
