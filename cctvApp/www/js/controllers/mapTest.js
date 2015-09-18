e19fdad7152bbfc2581679bc6f76ee05dc00321c
sky1core <sky1core@gmail.com>
2015-09-18 10:52:43 +0900

    MapTest 마커 변경

--- cctvApp/www/js/controllers/mapTest.js
+++ cctvApp/www/js/controllers/mapTest.js
@@ -10,12 +10,48 @@ angular.module('starter.controllers')
             var defLoc = soc.getDefaultLocation();
     		var mapOption = { 
         		center: new daum.maps.LatLng(defLoc.lat, defLoc.lon), // 지도의 중심좌표
+        		//center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         		level: 3 // 지도의 확대 레벨
 		   	};
 
 			// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 			var map = new daum.maps.Map(mapContainer, mapOption); 
 			
+            var markerImageSrc = 'img/map-pin_17x30.png'; // 마커이미지의 주소입니다    
+            var markerImageSize = new daum.maps.Size(15, 26); // 마커이미지의 크기입니다
+            var markerImageOption = {offset: new daum.maps.Point(7, 26)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다			
+			
+            // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+            var markerImage = new daum.maps.MarkerImage(markerImageSrc, markerImageSize, markerImageOption);
+            //var markerPosition = new daum.maps.LatLng(defLoc.lat, defLoc.lon); // 마커가 표시될 위치입니다			
+
+            var testMarkerPosition  = mapOption.center;
+
+            // 테스트용 고정 마커
+            var testMarker = new daum.maps.Marker({
+                position: testMarkerPosition,
+                image: markerImage
+            });
+            testMarker.setMap(map);
+            
+
+// Begin 임시 인포윈도우
+            // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+            var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+            // 인포윈도우를 생성합니다
+            var infowindow = new daum.maps.InfoWindow({
+                content : iwContent,
+                removable : iwRemoveable
+            });
+
+            // 마커에 클릭이벤트를 등록합니다
+            daum.maps.event.addListener(testMarker, 'click', function() {
+                  // 마커 위에 인포윈도우를 표시합니다
+                infowindow.open(map, testMarker);  
+            });
+// End 임시 인포윈도우
 
 
 			$scope.isDraging = false;
@@ -34,34 +70,50 @@ angular.module('starter.controllers')
 			    // 일단 지금은 실제 요청은 하지않고 테스트
 			    $scope.requestInfoCount += 1;
 			    var bounds = map.getBounds();
-			    
-			    // 실제 요청할때는 이 범위보다 2배(?) 큰범위를 요청한다
-			    $scope.requestInfoSW = bounds.getSouthWest().toString();
-			    $scope.requestInfoNE = bounds.getNorthEast().toString();			    
 
-			    $scope.requestInfoCenter = map.getCenter(); 
+                // ----------------------
+                
+                
+                var northEast   = bounds.getNorthEast();
+                var southWest   = bounds.getSouthWest();
+                var center      = map.getCenter();
+
+                var centerLng   = center.getLng();                
+                var centerLat   = center.getLat();
+
+                var east    = northEast.getLng();
+		        var north   = northEast.getLat();
+			    var west    = southWest.getLng();
+			    var south   = southWest.getLat();
 			    
-			    
+			    var width   = east - west;
+			    var height  = north - south;
+
                 // 서버측 API가 준비될때까지 ZoomLevel이 크면 요청을 하지 않는다			    
-			    if(map.getLevel() > 6) {
+			    if(map.getLevel() > 5) {
 			        return;
 			    }
 			    
-			    // 실제 요청하는 부분
+			    // 실제 요청 Parameter
+			    // 화면에 보이는 2배 요청
 			    var params = {
-			        east:   bounds.getNorthEast().getLng().toFixed(5),
-			        north:  bounds.getNorthEast().getLat().toFixed(5),
-			        west:   bounds.getSouthWest().getLng().toFixed(5),			        
-			        south:  bounds.getSouthWest().getLat().toFixed(5)
+			        east:   (centerLng + width  + 0.000005).toFixed(6),
+			        west:   (centerLng - width  - 0.000005).toFixed(6),			        
+			        north:  (centerLat + height + 0.000005).toFixed(6),
+			        south:  (centerLat - height - 0.000005).toFixed(6)
 			    }
-			  
+
+			    // 실제 요청할때는 이 범위보다 2배(?) 큰범위를 요청한다
+			    $scope.requestInfoSW = "(" + params.south + ", " + params.west + ")";
+			    $scope.requestInfoNE = "(" + params.north + ", " + params.east + ")";
+
+
+			    $scope.requestInfoCenter = map.getCenter();     // 이 변수는 다른곳에서 사용한다
+
+
 			    soc.getCctvs(params)
 			        .then(function(res) {
-                        //soc.log("SUCCESS: " + JSON.stringify(res));
-                        /*for (var cctv in cctvList) {
-                            var cctvId = cctvList[cctv].cctvId;
-                        }*/
-                        
+			            
                         // 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
                         soc.log("PREV length: " + markerList.length);
                         function deleteMarkers() {
@@ -83,7 +135,8 @@ angular.module('starter.controllers')
 
                                 // 마커를 생성합니다
                                 var marker = new daum.maps.Marker({
-                                    position: markerPosition
+                                    position: markerPosition,
+                                    image: markerImage
                                 });
                             
                                 // 마커가 지도 위에 표시되도록 설정합니다
@@ -97,10 +150,13 @@ angular.module('starter.controllers')
                         }
                         soc.log("AFT length: " + markerList.length);
                         
+			            $scope.responseInfoCount = map.getCenter();
+                        
                     }, function(err) {
                         soc.log("ERROR: " + JSON.stringify(err));
                     }
                 );
+                
 			}
 
 			// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
