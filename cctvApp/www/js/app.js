6e8e8604dba3699505b1d7a596b0668f187350b5
CloverHearts <estail7s@gmail.com>
2015-09-23 23:53:21 +0900

    fix splash white

--- cctvApp/www/js/app.js
+++ cctvApp/www/js/app.js
@@ -10,6 +10,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    setTimeout(function() {
+      $cordovaSplashscreen.hide();
+    }, 500);
+    
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
@@ -17,6 +21,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
+
       StatusBar.styleDefault();
     }
     
