af3a690997ded950020f604063667bbf1eb6ad5e
CloverHearts <estail7s@gmail.com>
2015-09-24 01:07:20 +0900

    test

--- cctvApp/www/js/app.js
+++ cctvApp/www/js/app.js
@@ -11,7 +11,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     setTimeout(function() {
-      $cordovaSplashscreen.hide();
+      navigator.splashscreen.hide();
     }, 500);
 
     if (window.cordova && window.cordova.plugins.Keyboard) {
