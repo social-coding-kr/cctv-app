27e26c103a0cdc28be9965a51b7f781fb3d2a564
CloverHearts <estail7s@gmail.com>
2015-09-24 02:48:47 +0900

    fix Cordova plugin

--- cctvApp/www/js/app.js
+++ cctvApp/www/js/app.js
@@ -10,6 +10,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    setTimeout(function() {
+      navigator.splashscreen.hide();
+    }, 500);
 
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -128,13 +131,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
   
   $stateProvider
-
+/*
       .state('appstart', {
         url: '/appstart',
         templateUrl: 'templates/splash.html',
         controller: 'AppSplash'
       })
-
+*/
     .state('app', {
     url: '/app',
     abstract: true,
@@ -320,11 +323,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/appstart');
-  //$urlRouterProvider.otherwise('/app/map');
+  //$urlRouterProvider.otherwise('/appstart');
+  $urlRouterProvider.otherwise('/app/map');
 
 })
-
+/*
 .controller('AppSplash', function($scope, $location, $interval) {
 
       // this controller is dead or sleep
@@ -348,3 +351,4 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
 
 });
+*/
