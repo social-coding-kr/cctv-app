4209fa6aebc18afce31c2f9782a039ad4733b08f
CloverHearts <estail7s@gmail.com>
2015-09-24 00:14:12 +0900

    whitle

--- cctvApp/www/js/app.js
+++ cctvApp/www/js/app.js
@@ -10,9 +10,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    document.addEventListener('deviceready', function() {
-      navigator.splashscreen.hide();
-    });
+
+    $cordovaSplashscreen.show();
 
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
