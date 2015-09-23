af1da38fb02e24adce88c6b6ef9b585c4769b764
CloverHearts <estail7s@gmail.com>
2015-09-23 23:57:54 +0900

    fix not defined cordova splash plugin

--- cctvApp/www/js/app.js
+++ cctvApp/www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
-.run(function($ionicPlatform, $rootScope, soc) {
+.run(function($ionicPlatform, $cordovaSplashscreen, $rootScope, soc) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
