08aedcf1dcdc5a3ee7cb5503ec8e7c337f2f9f31
sky1core <sky1core@gmail.com>
2015-10-18 02:24:58 +0900

    코르도바 플러그인 Diagnostic 추가

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cordova plugin | awk '{ print $1 }' | xargs ionic plugin rm
 cordova plugin add cordova-plugin-file
 cordova plugin add cordova-plugin-file-transfer
 cordova plugin add cordova-plugin-camera
-cordova plugin add org.apache.cordova.geolocation
+cordova plugin add cordova-plugin-geolocation
 cordova plugin add https://github.com/apache/cordova-plugin-whitelist.git
 cordova plugin add https://github.com/pwlin/cordova-plugin-file-opener2.git
 cordova plugin add https://github.com/Initsogar/cordova-webintent.git
@@ -16,6 +16,7 @@ cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
 cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-inappbrowser.git
 cordova plugin add https://github.com/cloverhearts/cordova-plugin-splashscreen
 cordova plugin add cordova-plugin-network-information
+cordova plugin add https://github.com/dpa99c/cordova-diagnostic-plugin.git
 
 ionic platform rm android
 ionic platform add android
