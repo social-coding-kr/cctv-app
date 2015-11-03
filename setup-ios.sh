ae753a7ad879c8f6d2d00d44e3460a65fb9d48dc
신규연 <shinkyuyeon@singyuyeon-ui-MacBook-Pro.local>
2015-11-03 15:34:38 +0900

    IOS setup 추가

---
+++ setup-ios.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pushd cctvApp
+cordova plugin | awk '{ print $1 }' | xargs ionic plugin rm
+cordova plugin | awk '{ print $1 }' | xargs ionic plugin rm
+
+
+cordova plugin add cordova-plugin-file
+cordova plugin add cordova-plugin-file-transfer
+cordova plugin add cordova-plugin-camera
+cordova plugin add cordova-plugin-geolocation
+cordova plugin add https://github.com/apache/cordova-plugin-whitelist.git
+cordova plugin add https://github.com/pwlin/cordova-plugin-file-opener2.git
+cordova plugin add https://github.com/Initsogar/cordova-webintent.git
+cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-inappbrowser.git
+cordova plugin add https://github.com/cloverhearts/cordova-plugin-splashscreen
+cordova plugin add cordova-plugin-network-information
+cordova plugin add https://github.com/dpa99c/cordova-diagnostic-plugin.git
+cordova plugin add cordova-plugin-dialogs
+cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard.git
+
+ionic platform rm ios
+ionic platform add ios
